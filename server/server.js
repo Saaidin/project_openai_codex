defbd1c464e6e12a7ffc9be4d13e857ea68687a3
saaidin <dinme73@gmail.com>
2023-02-11 01:06:08 +0800

    update-110223

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from "express"
+import * as dotenv from "dotenv"
+import cors from "cors"
+import { Configuration, OpenAIApi } from "openai"
 
 dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
+})
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration)
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: "Hello from CodeX!",
   })
 })
 
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const prompt = req.body.prompt
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
@@ -33,16 +33,17 @@ app.post('/', async (req, res) => {
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    });
+    })
 
     res.status(200).send({
-      bot: response.data.choices[0].text
-    });
-
+      bot: response.data.choices[0].text,
+    })
   } catch (error) {
     console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    res.status(500).send(error || "Something went wrong")
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () =>
+  console.log("AI server started on http://localhost:5000")
+)
